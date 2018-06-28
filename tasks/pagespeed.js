95004e2dc14c1dbf12a4f6ead9ae51ee05977d46
Samuel Fernandez <SFernandez@jw.org>
2018-06-28 11:58:33 +0200

    Last updates

--- tasks/pagespeed.js
+++ tasks/pagespeed.js
@@ -5,6 +5,19 @@ const argv = require('yargs').argv;
 const psi = require('psi');
 const comment = require('../lib/github').comment;
 
+// PageSpeed API options
+const mobileOptions = {
+  nokey: true,
+  strategy: 'mobile',
+  threshold: 0
+};
+const desktopOptions = {
+  nokey: true,
+  strategy: 'desktop',
+  threshold: 0
+};
+
+
 gulp.task('pagespeed', () => {
 
   // Get task parameters
@@ -21,63 +34,63 @@ gulp.task('pagespeed', () => {
     return Promise.reject(new Error(msg));
   }
 
-  // PageSpeed API options
-  const mobileOptions = { nokey: true, strategy: 'mobile', threshold: 0 };
-  const desktopOptions = { nokey: true, strategy: 'desktop', threshold: 0 };
-
   console.log(chalk.green(`Launching PageSpeed Insights audit for url: ${url}`));
 
   // Get the PageSpeed Insights report
-  let mobileResult = {};
-  let desktopResult = {};
 
   return Promise.all([psi.output(url, mobileOptions), psi.output(url, desktopOptions)]) // Console report
 
     // JSON results
     .then(() => Promise.all([psi(url, mobileOptions), psi(url, desktopOptions)]))
 
-    // Save results
+    // Process results results
     .then((psiResults) => {
-      [mobileResult, desktopResult] = psiResults;
+
+      const [mobileResult, desktopResult] = psiResults;
+
+      mobileResult.failedTreshold = treshold && mobileResult.ruleGroups.SPEED.score < treshold;
+      desktopResult.failedTreshold = treshold && desktopResult.ruleGroups.SPEED.score < treshold;
+
+      return [mobileResult, desktopResult];
     })
 
     // Post comment to GitHub with audit results
-    .then(() => {
+    .then(([mobileResult, desktopResult]) => {
 
       // Skip comment if parameter not passed
       if (!github) {
-        return;
+        return [mobileResult, desktopResult];
       }
 
       const title = 'PageSpeed Insights';
       const reportUrl = `https://developers.google.com/speed/pagespeed/insights/?url=${encodeURI(url)}`;
       const logoUrl = 'https://www.gstatic.com/images/icons/material/product/2x/pagespeed_64dp.png';
-      const body = `<img src="${logoUrl}" height="80px">\n\n` +
+      let body = `<img src="${logoUrl}" height="80px">\n\n` +
         `Report at [${reportUrl}](${reportUrl})\n\n` +
         `* Mobile Speed: ${mobileResult.ruleGroups.SPEED.score}%\n` +
         `* Desktop Speed: ${desktopResult.ruleGroups.SPEED.score}%`;
 
-      return comment(title, body);
+      if (treshold) {
+         if (mobileResult.failedTreshold || desktopResult.failedTreshold) {
+            body += "```diff\n---Audit failed with score <" + treshold + "%\n```";
+         } else {
+            body += "```diff\n+++Audit succeeded with score >" + treshold + "%\n```";
+         }
+      }
+
+      return comment(title, body).then(() => [mobileResult, desktopResult]);
     })
 
     // Check treshold
-    .then(() => {
+    .then(([mobileResult, desktopResult]) => {
 
-      const failedTresholdMobile = treshold && mobileResult.ruleGroups.SPEED.score < treshold;
-      const failedTresholdDesktop = treshold && desktopResult.ruleGroups.SPEED.score < treshold;
-
-      if (failedTresholdMobile || failedTresholdDesktop) {
+      if (mobileResult.failedTreshold || desktopResult.failedTreshold) {
         return Promise.reject(new Error(
           `PageSpeed Insights treshold ${treshold}% not met ` +
           `with mobile score ${mobileResult.ruleGroups.SPEED.score}% and ` +
           `desktop score ${desktopResult.ruleGroups.SPEED.score}%.`
         ));
       }
-    })
 
-    // Print errors to the console and fail
-    /*.catch((err) => {
-      console.error(chalk.bold.red(err));
-      done(err); // Exit process with failure
-    });*/
+    });
 });
