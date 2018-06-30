947fd419075417dc333359147187173ab2040189
Samuel <samuel.fernandez@gmail.com>
2018-06-30 20:38:32 +0200

    Commit add linter for js

--- tasks/lighthouse.js
+++ tasks/lighthouse.js
@@ -1,147 +1,154 @@
 const Promise = require('bluebird');
 const gulp = require('gulp');
 const chalk = require('chalk');
-const argv = require('yargs').argv;
-// const lighthousePrinter = require('lighthouse/lighthouse-cli/printer');
+const { argv } = require('yargs');
+// Const lighthousePrinter = require('lighthouse/lighthouse-cli/printer');
 const lighthouse = require('lighthouse');
 const chromeLauncher = require('chrome-launcher');
-const comment = require('../lib/github').comment;
+const { comment } = require('../lib/github');
 
 // Lighthouse categories
 const categories = ['performance', 'pwa', 'accessibility', 'best-practices', 'seo'];
 
 // Lighthouse key audits
 const performanceAudits = [
-  'first-meaningful-paint',
-  'first-cpu-idle',
-  'interactive',
-  'estimated-input-latency',
-  'speed-index'
+   'first-meaningful-paint',
+   'first-cpu-idle',
+   'interactive',
+   'estimated-input-latency',
+   'speed-index'
 ];
 
 // Helper functions
-const getAverageValue = (collection, getValue) => collection
-  .map((item) => getValue(item))
-  .reduce((average, value) => average + value / collection.length, 0)
-  .toFixed();
+const getAverageValue = (collection, getValue) =>
+   collection
+      .map(item => getValue(item))
+      .reduce((average, value) => average + value / collection.length, 0)
+      .toFixed();
 
 // Method to run Lighthouse audits
-const launchChromeAndRunLighthouse = (url) => {
-  return chromeLauncher.launch({
-      chromeFlags: ['--headless', '--no-sandbox']
-    })
-    .then(chrome => {
-      console.log('   Chrome launched, starting audit...');
-      console.time('   Lighthouse audit finished in');
-      const flags = {
-        port: chrome.port
-      };
-      return lighthouse(url, flags, null).then(results => {
-        console.timeEnd('   Lighthouse audit finished in');
-        console.log();
-        return chrome.kill().then(() => results).catch(() => results);
+const launchChromeAndRunLighthouse = url => {
+   return chromeLauncher
+      .launch({
+         chromeFlags: ['--headless', '--no-sandbox']
+      })
+      .then(chrome => {
+         console.log('   Chrome launched, starting audit...');
+         console.time('   Lighthouse audit finished in');
+         const flags = {
+            port: chrome.port
+         };
+         return lighthouse(url, flags, null).then(results => {
+            console.timeEnd('   Lighthouse audit finished in');
+            console.log();
+            return chrome
+               .kill()
+               .then(() => results)
+               .catch(() => results);
+         });
       });
-    });
 };
 
+gulp.task('lighthouse', () => {
+   // Get task parameters
+   const { url, threshold, github } = argv;
+   const runs = Math.min(Math.max(Number(argv.runs) || 1, 1), 5);
 
-gulp.task('lighthouse', function () {
+   // Validate url
+   const validUrl = /^http(s)?:\/\//;
 
-  // Get task parameters
-  const { url, threshold, github } = argv;
-  const runs = Math.min(Math.max(Number(argv.runs) || 1, 1), 5);
+   if (!url || !validUrl.test(url)) {
+      const msg = 'Lighthouse task needs to be called with a valid --url.';
+      console.error(chalk.bold.red(msg));
+      return Promise.reject(new Error(msg));
+   }
 
-  // Validate url
-  const validUrl = /^http(s)?:\/\//;
+   console.log(chalk.green(`Launching Lighthouse audit for url: ${url}`));
 
-  if (!url || !validUrl.test(url)) {
-    const msg = 'Lighthouse task needs to be called with a valid --url.';
-    console.error(chalk.bold.red(msg));
-    return Promise.reject(new Error(msg));
-  }
+   // Get the Lighthouse report
 
-  console.log(chalk.green(`Launching Lighthouse audit for url: ${url}`));
+   return (
+      Promise.mapSeries(
+         [...new Array(runs)], // Iterate over the number of runs...
+         () => launchChromeAndRunLighthouse(argv.url) // ...generating audits in sequence
+      )
 
-  // Get the Lighthouse report
+         // Get the Lighthouse results
+         .then(lighthouseResults => {
+            console.log(chalk.green('Audit finished with the following results:'));
 
-  return Promise.mapSeries(
-      Array.from(Array(runs)), // Iterate over the number of runs...
-      () => launchChromeAndRunLighthouse(argv.url) // ...generating audits in sequence
-    )
+            const lastResult = lighthouseResults[lighthouseResults.length - 1];
 
-    // Get the Lighthouse results
-    .then((lighthouseResults) => {
+            categories.map(category => lastResult.lhr.categories[category]).forEach(category => {
+               // Log report
+               const meanScore = getAverageValue(
+                  lighthouseResults,
+                  result => result.lhr.categories[category.id].score * 100
+               );
+               console.log(chalk.yellow(`   ${category.title}: ${meanScore}%`));
 
-      console.log(chalk.green('Audit finished with the following results:'));
+               // Calculate mean results for performance audits
+               if (category.id !== 'performance') {
+                  return;
+               }
 
-      const lastResult = lighthouseResults[lighthouseResults.length - 1];
-
-      categories
-        .map((category) => lastResult.lhr.categories[category])
-        .forEach((category) => { // Log report
-          const meanScore = getAverageValue(lighthouseResults, (result) => result.lhr.categories[category.id].score * 100);
-          console.log(chalk.yellow(`   ${category.title}: ${meanScore}%`));
-
-          // Calculate mean results for performance audits
-          if (category.id !== 'performance') {
-            return;
-          }
-
-          performanceAudits
-            .map((audit) => lastResult.lhr.audits[audit])
-            .forEach((audit) => {
-              const meanRawValue = getAverageValue(lighthouseResults, (result) => result.lhr.audits[audit.id].rawValue);
-              return console.log(chalk.yellow(`      ${audit.title}: ${meanRawValue} ms`));
+               performanceAudits.map(audit => lastResult.lhr.audits[audit]).forEach(audit => {
+                  const meanRawValue = getAverageValue(
+                     lighthouseResults,
+                     result => result.lhr.audits[audit.id].rawValue
+                  );
+                  return console.log(chalk.yellow(`      ${audit.title}: ${meanRawValue} ms`));
+               });
             });
-        });
 
-      return lastResult;
-    })
+            return lastResult;
+         })
 
-    // Post comment to GitHub with audit results
-    .then((lastResult) => {
+         // Post comment to GitHub with audit results
+         .then(lastResult => {
+            // Skip comment if parameter not passed
+            if (!github) {
+               return lastResult;
+            }
 
-      // Skip comment if parameter not passed
-      if (!github) {
-        return lastResult;
-      }
+            const title = 'Lighthouse Audit';
+            const logoUrl = 'https://github.com/GoogleChrome/lighthouse/raw/master/assets/crx/lighthouse-small.jpg';
+            let body = `<img src="${logoUrl}" height="80px">\n\nDeployed at [${url}](${url})\n\n`;
 
-      const title = 'Lighthouse Audit';
-      const logoUrl = 'https://github.com/GoogleChrome/lighthouse/raw/master/assets/crx/lighthouse-small.jpg';
-      let body = `<img src="${logoUrl}" height="80px">\n\nDeployed at [${url}](${url})\n\n`;
+            body += categories
+               .map(category => lastResult.lhr.categories[category])
+               .map(category => {
+                  const categoryBody = `* ${category.title}: ${Math.floor(category.score * 100)}%`;
 
-      body += categories
-        .map((category) => lastResult.lhr.categories[category])
-        .map((category) => {
-          const categoryBody = `* ${category.title}: ${parseInt(category.score * 100)}%`;
+                  const auditsBody =
+                     category.id !== 'performance'
+                        ? []
+                        : performanceAudits
+                             .map(audit => lastResult.lhr.audits[audit])
+                             .map(audit => `  * ${audit.title}: ${Math.floor(audit.rawValue)} ms`);
 
-          const auditsBody = category.id !== 'performance' ? [] : performanceAudits
-            .map((audit) => lastResult.lhr.audits[audit])
-            .map((audit) => `  * ${audit.title}: ${parseInt(audit.rawValue)} ms`);
+                  return [categoryBody, ...auditsBody].join('\n');
+               })
+               .join('\n');
 
-          return [categoryBody, ...auditsBody].join('\n');
-        }).join('\n');
+            if (threshold) {
+               if (lastResult.score < threshold) {
+                  body += '\n\n```diff\n--- Audit failed with score < ' + threshold + '%\n```';
+               } else {
+                  body += '\n\n```diff\n+++ Audit succeeded with score > ' + threshold + '%\n```';
+               }
+            }
 
-      if (threshold) {
-        if (lastResult.score < threshold) {
-          body += "\n\n```diff\n--- Audit failed with score < " + threshold + "%\n```";
-        } else {
-          body += "\n\n```diff\n+++ Audit succeeded with score > " + threshold + "%\n```";
-        }
-      }
+            return comment(title, body).then(() => lastResult);
+         })
 
-      return comment(title, body).then(() => lastResult);
-    })
-
-    // Check threshold
-    .then((lastResult) => {
-
-      if (threshold && lastResult.score < threshold) {
-        return Promise.reject(new Error(
-          `Lighthouse audit failed with score ${parseInt(lastResult.score)}% (<${parseInt(threshold)}%).`
-        ));
-      }
-
-    });
-
+         // Check threshold
+         .then(lastResult => {
+            if (threshold && lastResult.score < threshold) {
+               return Promise.reject(
+                  new Error(`Lighthouse audit failed with score ${Math.floor(lastResult.score)}% (<${threshold}%).`)
+               );
+            }
+         })
+   );
 });
