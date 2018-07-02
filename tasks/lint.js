af3ba6f080138e168936543133e138dac7591f4d
Samuel Fernandez <SFernandez@jw.org>
2018-07-02 13:10:16 +0200

    HTML lint

--- tasks/lint.js
+++ tasks/lint.js
@@ -2,7 +2,8 @@ const gulp = require('gulp');
 const run = require('gulp-run-command').default;
 
 gulp.task('lint:js', run('eslint . --ignore-path .gitignore'));
-gulp.task('lint:scss', run('stylelint "**/*.scss" --ignore-path .gitignore'));
+gulp.task('lint:scss', run('stylelint "src/**/*.scss" --ignore-path .gitignore'));
 gulp.task('lint:ts', run('ng lint'));
+gulp.task('lint:html', run('htmllint "src/**/*.html"'));
 
-gulp.task('lint', gulp.parallel('lint:js', 'lint:scss', 'lint:ts'));
+gulp.task('lint', gulp.parallel('lint:js', 'lint:scss', 'lint:ts', 'lint:html'));
