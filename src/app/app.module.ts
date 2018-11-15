a70a64e61fe411db6ca1ac3a0a7e3a7b969e8cda
Samuel Fernández <samuelfernandez@users.noreply.github.com>
2018-11-15 11:59:07 +0100

    feat(PWA): Implement PWA (#52)
    * feat(PWA): Implement PWA
    
    * chore(commitlint): replace validate-commit-msg by commitlint
    
    * chore(husky): move settings to new format at husky.config.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { ShellComponent, ShellModule } from './shell';
 
 @NgModule({
-   imports: [BrowserModule.withServerTransition({ appId: 'serverApp' }), ShellModule, AppRoutingModule],
+   imports: [
+      BrowserModule.withServerTransition({ appId: 'serverApp' }),
+      ShellModule,
+      AppRoutingModule,
+      ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+   ],
    exports: [ShellModule],
    bootstrap: [ShellComponent]
 })
--- tasks/optimize.js
+++ tasks/optimize.js
@@ -1,3 +1,8 @@
 const gulp = require('gulp');
+const run = require('gulp-run-command').default;
 
-gulp.task('optimize', gulp.series('optimize:critical', 'optimize:css', 'optimize:html', 'optimize:images'));
+// After running the optimizations, the assets have changed, so we need to update the service worker metadata
+gulp.task('optimize:ngsw', run('ngsw-config dist/portfolio ngsw-config.json'));
+
+const tasks = gulp.series('optimize:critical', 'optimize:css', 'optimize:html', 'optimize:images', 'optimize:ngsw');
+gulp.task('optimize', tasks);
